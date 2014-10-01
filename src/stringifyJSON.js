0c41ae56bda4053aae2c6f07cd7ca7714fde5f2e
Rustin Crandall <rustinpc@gmail.com>
2014-09-30 20:44:43 -0700

    Got stringifyJSON to pass.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,45 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "string") {
+  	return '"' + obj + '"';
+  }
+  else if (Array.isArray(obj)) {
+  	if (obj.length < 1) {
+  		return "[]";
+  	}
+  	else if (obj.length = 1) {
+  		return '[' + stringifyJSON(obj[0]) + ']';
+  	}
+  	else {
+  		var arrayString = "";
+  		for (var i=0; i<obj.length -1; i++) {
+  			arrayString += stringifyJSON(obj[i]) + ',';
+  		}
+  		arrayString += stringifyJSON(obj[obj.length - 1]);
+  	    return '[' + arrayString + ']';
+  	}
+  }
+  else if (obj === null) {
+  	return "null";
+  }
+  else if (typeof obj === "object") {
+  	var objectString = "{";
+  	for (var key in obj) {
+  		if (typeof obj[key] === "function") {
+  			return "{}";
+  		}
+  		else {
+  			objectString += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+  		}
+  	}
+  	if (objectString !== "{") {
+  	    objectString = objectString.slice(0,objectString.length - 1);
+  	}
+  	objectString += "}";
+  	return objectString;
+  }
+  else {
+    return String(obj);
+  }
 };
